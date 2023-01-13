0f6ab6b00bcf259a4e15ffe781449a3ffe1c83fc
Екатерина Савченко <KattySavchenko1@yandex.ru>
2023-01-13 19:37:16 +0200

    Финальное задание

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -38,8 +38,7 @@ def group_posts(request, slug):
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     posts = author.posts.select_related("author", "group")
-    following = (request.user.is_authenticated
-                 and author.following.filter(user=request.user).exists())
+    following = author.following.exists()
     context = {
         "author": author,
         "page_obj": paginate_page(request, posts),
